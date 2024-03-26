08e1c27d1436e489ca29757776858c40a4e9cb56
Picklechin87 <raaugustine@csuchico.edu>
2024-03-26 09:35:53 -0700

    Health

--- Grapple/Assets/Scripts/Health.cs
+++ Grapple/Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
 {
     public int maxhealth = 3;
     public int currenthealth;
+    public int amount;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +24,18 @@ public class Health : MonoBehaviour
         }
     }
 
-    private void On
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Hazard")
+        {
+            TakeDamage(1);
+        }
+    }
+ 
+   
+        // Update is called once per frame
+        void Update()
+    {
+           
     }
 }
