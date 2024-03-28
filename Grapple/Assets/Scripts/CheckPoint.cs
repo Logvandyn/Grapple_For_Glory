98fcf7ca8739ac5c961e3aa2c9d3b748fb53847c
banana21218 <113222030+banana21218@users.noreply.github.com>
2024-03-27 21:28:03 -0700

    checkpoint

---
+++ Grapple/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "CheckPoint")
+        {
+            transform.position = startPosition;
+            Blink();
+            GetComponent<Transform>().position = startPosition;
+        }
+    }
+    //cause the player to blink
+    public IEnumerator Blink()
+    {
+        for (int index = 0; index < 20; index++)
+        {
+            if (index % 2 == 0)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+        GetComponent<MeshRenderer>().enabled = true;
+    }
+}
