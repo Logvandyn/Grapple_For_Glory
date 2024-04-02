a8c06316495b51549febe88f0438eae46d430516
Picklechin87 <raaugustine@csuchico.edu>
2024-04-02 13:02:07 -0700

    Updates

--- Grapple/Assets/Scripts/Health.cs
+++ Grapple/Assets/Scripts/Health.cs
@@ -14,6 +14,8 @@ public class Health : MonoBehaviour
     public Vector3 startPosition; //for spawning / respawning
     public TextMeshProUGUI healthcounter;
     public TextMeshProUGUI livescounter;
+    public TextMeshProUGUI lose;
+    public TextMeshProUGUI win;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,6 +46,10 @@ public class Health : MonoBehaviour
         {
             Respawn();
         }
+        if (other.gameObject.tag == "Win")
+        {
+            win.text = "YOU WIN!";
+        }
     }
  
    
@@ -58,6 +64,10 @@ public class Health : MonoBehaviour
         //counts the Health you have left
         healthcounter.text = "Health " + currenthealth.ToString();
         livescounter.text = "Lives " + currentlives.ToString();
+        if (currentlives <= 0)
+        {
+            lose.text = "GAME OVER";
+        }
 
     }
     private void Respawn()
