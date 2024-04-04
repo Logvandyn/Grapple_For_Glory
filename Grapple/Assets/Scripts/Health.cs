e8a1f9f7b0cf7383d0e8efb3e8eb3d7cf7d3643d
Logvandyn <logvandyn@yahoo.com>
2024-04-04 08:01:01 -0700

    Squashed commit of the following:
    commit 2ade1772cc9bc781194c066702e5385bf82ae08c
    Author: Picklechin87 <raaugustine@csuchico.edu>
    Date:   Tue Apr 2 09:15:54 2024 -0700
    
        Squashed commit of the following:
    
        commit 1d24e161c2e5779264602872266eda7f8bdd0b3e
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Apr 2 09:15:07 2024 -0700
    
            shit van shart
    
        commit 0531df074d45b8cfa7aa9031f2460251e3d28cd5
        Merge: 1dfaf14 4f272c0
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Apr 2 09:07:29 2024 -0700
    
            Merge branch 'main' into Ryan
    
        commit 1dfaf147cf814f5d01f72e17f7c86f684948fca3
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Apr 2 09:06:57 2024 -0700
    
            lava
    
        commit 544783e3a0afd258122c5a5aa9a40269ecc6754e
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Apr 2 08:33:03 2024 -0700
    
            FecesVanFungledungle
    
        commit 735c1c368f7f9889bb2d9c1d967f3345cbf00665
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Mar 26 11:23:12 2024 -0700
    
            lives
    
        commit 9411906de50a9534f495b6ea22dc3d6bd739d2cc
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Mar 26 10:58:41 2024 -0700
    
            healthUi
    
        commit e0a4b0758f89b70594663dd6f85cf16632fcb2a5
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Mar 26 09:44:04 2024 -0700
    
            more stuff
    
        commit 08e1c27d1436e489ca29757776858c40a4e9cb56
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Mar 26 09:35:53 2024 -0700
    
            Health
    
        commit 65904af4b44642d9733b8da37d8329dfd8d44eff
        Author: Picklechin87 <raaugustine@csuchico.edu>
        Date:   Tue Mar 26 09:19:09 2024 -0700
    
            switch Branch

--- Grapple/Assets/Scripts/Health.cs
+++ Grapple/Assets/Scripts/Health.cs
@@ -1,25 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public int maxhealth = 3;
     public int currenthealth;
+    public int maxlives = 3;
+    public int currentlives;
+    public int amount;
+    private Vector3 startPosition; //for spawning / respawning
+    public TextMeshProUGUI healthcounter;
+    public TextMeshProUGUI livescounter;
     // Start is called before the first frame update
     void Start()
     {
         currenthealth = maxhealth;
+        currentlives = maxlives;
+        SetCountText();
     }
 
     public void TakeDamage(int amount)
     {
+        currenthealth -= amount;
 
+        if (currenthealth <= 0)
+        {
+            //Dead
+            Respawn();
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Hazard")
+        {
+            TakeDamage(1);
+        }
+        if (other.gameObject.tag == "Lava")
+        {
+            Respawn();
+        }
+    }
+ 
+   
+        // Update is called once per frame
+        void Update()
+    {
+        SetCountText();
+    }
+
+    public void SetCountText()
+    {
+        //counts the Health you have left
+        healthcounter.text = "Health " + currenthealth.ToString();
+        livescounter.text = "Lives " + currentlives.ToString();
+
+    }
+    private void Respawn()
+    {
+            transform.position = startPosition;
+        currenthealth = maxhealth;
+        currentlives -= 1;
     }
 }
